7f571a90e0592ad319130b36ad2b03b58dd52fc3
JuanPatino <josejpdd8@gmail.com>
2023-09-23 23:43:42 -0500

    gateway

--- data-micro-serv/src/main/java/com/example/datamicroserv/DataMicroServApplication.java
+++ data-micro-serv/src/main/java/com/example/datamicroserv/DataMicroServApplication.java
@@ -2,7 +2,10 @@ package com.example.datamicroserv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @SpringBootApplication
+@RequestMapping("/data")
 public class DataMicroServApplication {
 
 	public static void main(String[] args) {
--- web-scraping-pasaportes/src/main/java/com/example/webscrapingpasaportes/WebScrapingPasaportesApplication.java
+++ web-scraping-pasaportes/src/main/java/com/example/webscrapingpasaportes/WebScrapingPasaportesApplication.java
@@ -2,8 +2,10 @@ package com.example.webscrapingpasaportes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RequestMapping("/web-scraping")
 public class WebScrapingPasaportesApplication {
 
 	public static void main(String[] args) {
