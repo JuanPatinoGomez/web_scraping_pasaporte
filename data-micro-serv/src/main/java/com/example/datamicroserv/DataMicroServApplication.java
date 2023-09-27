bda3463006417eb976922146aedc6b4c400e85f5
JuanPatino <josejpdd8@gmail.com>
2023-09-26 21:57:48 -0500

    gateway

--- data-micro-serv/src/main/java/com/example/datamicroserv/DataMicroServApplication.java
+++ data-micro-serv/src/main/java/com/example/datamicroserv/DataMicroServApplication.java
@@ -2,10 +2,8 @@ package com.example.datamicroserv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RequestMapping("/data-micro")
 public class DataMicroServApplication {
 
 	public static void main(String[] args) {
