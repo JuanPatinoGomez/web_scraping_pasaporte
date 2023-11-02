ce8d10d5669ac06da477b100520da4480bb32e5e
JuanPatino <josejpdd8@gmail.com>
2023-11-01 21:28:51 -0500

    Se crea microservicio de contendra las funcionalidades relacionadas a excell

---
+++ excell-lector/src/test/java/com/example/excelllector/ExcellLectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excelllector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcellLectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
